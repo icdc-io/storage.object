1162ddb9459e354ea16bb12882513d0eb739474e
Pavelkv94 <pavel.kv94@yandex.by>
2022-12-26 16:17:41 +0300

    fix member role avails

--- src/Components/UserDetails/Resources/editResModal.js
+++ src/Components/UserDetails/Resources/editResModal.js
@@ -9,34 +9,35 @@ import { editS3userAndFetch } from '../../../AppActions';
 import { BILLING_USER_NAME } from '../../../AppConstants';
 import EditResForm from './editResForm';
 
-const mapPropsToApi = (item) => ({
-    description: item.description,
-    owner: item.owner || '',
-    limits: {
-        storage_size: +item.storageSizeLimit,
-        objects: +item.objectsLimit,
-        max_buckets: +item.bucketsLimit,
-        bucket_storage_size: +item.storageInBucketLimit,
-        bucket_objects: +item.objectsInBucketLimit,
-    },
-});
-
-const mapApiToProps = (item) => ({
-    name: item.name,
-    description: item.description,
-    default_placement: item.default_placement?.id,
-    storageSizeLimit: item.stats?.storage_size.limit || 0,
-    objectsLimit: item.stats?.objects.limit || 0,
-    bucketsLimit: +item.stats?.buckets.limit || 0,
-    storageInBucketLimit: item.stats?.storage_bucket_limit,
-    objectsInBucketLimit: item.stats?.object_bucket_limit,
-    owner: item.owner,
-});
-
 const EditResModal = ({ t, s3user, name, label }) => {
     const dispatch = useDispatch();
     const [open, setOpen] = useState(false);
     const userRole = useSelector((state) => state.host.user.role);
+    const currentOwner = useSelector((state) => state.host.user.email);
+
+    const mapPropsToApi = (item) => ({
+        description: item.description,
+        owner: item.owner || currentOwner,
+        limits: {
+            storage_size: +item.storageSizeLimit,
+            objects: +item.objectsLimit,
+            max_buckets: +item.bucketsLimit,
+            bucket_storage_size: +item.storageInBucketLimit,
+            bucket_objects: +item.objectsInBucketLimit,
+        },
+    });
+
+    const mapApiToProps = (item) => ({
+        name: item.name,
+        description: item.description,
+        default_placement: item.default_placement?.id,
+        storageSizeLimit: item.stats?.storage_size.limit || 0,
+        objectsLimit: item.stats?.objects.limit || 0,
+        bucketsLimit: +item.stats?.buckets.limit || 0,
+        storageInBucketLimit: item.stats?.storage_bucket_limit,
+        objectsInBucketLimit: item.stats?.object_bucket_limit,
+        owner: item.owner,
+    });
 
     const handleClose = useCallback(() => {
         setOpen(false);
@@ -62,14 +63,7 @@ const EditResModal = ({ t, s3user, name, label }) => {
                 <Modal open={open} size="tiny" onSubmit={onSubmit}>
                     <Header content={label} />
                     <Modal.Content>
-                        <EditResForm
-                            t={t}
-                            open={open}
-                            handleClose={handleClose}
-                            onSubmit={onSubmit}
-                            name={name}
-                            initialValues={mapApiToProps(s3user)}
-                        />
+                        <EditResForm t={t} open={open} handleClose={handleClose} onSubmit={onSubmit} name={name} initialValues={mapApiToProps(s3user)} />
                     </Modal.Content>
                 </Modal>
             </React.Fragment>
--- src/Components/Users/userModal.js
+++ src/Components/Users/userModal.js
@@ -9,51 +9,52 @@ import { actionAndFetch, createS3user, editS3userAndFetch } from '../../AppActio
 import { BILLING_USER_NAME } from '../../AppConstants';
 import UserForm from './userForm';
 
-const mapPropsToApi = (item, edit) =>
-    edit
-        ? {
-              description: item.description,
-              owner: item.owner || '',
-              limits: {
-                  storage_size: +item.storageSizeLimit,
-                  objects: +item.objectsLimit,
-                  max_buckets: +item.bucketsLimit,
-                  bucket_storage_size: +item.storageInBucketLimit,
-                  bucket_objects: +item.objectsInBucketLimit,
-              },
-          }
-        : {
-              name: item.name,
-              description: item.description,
-              owner: item.owner || '',
-              default_placement: item.storageType,
-              limits: {
-                  storage_size: +item.storageSizeLimit,
-                  objects: +item.objectsLimit,
-                  max_buckets: +item.bucketsLimit,
-                  bucket_storage_size: +item.storageInBucketLimit,
-                  bucket_objects: +item.objectsInBucketLimit,
-              },
-          };
-
-const mapApiToProps = (item) => ({
-    name: item.name,
-    description: item.description,
-    default_placement: item.default_placement.id,
-    storageSizeLimit: item.stats.storage_size?.limit || 0,
-    objectsLimit: item.stats.objects?.limit || 0,
-    bucketsLimit: +item.stats.buckets?.limit || 0,
-    storageInBucketLimit: item.stats.storage_bucket_limit,
-    objectsInBucketLimit: item.stats.object_bucket_limit,
-    owner: item.owner,
-});
-
 const UserModal = ({ user, edit, t }) => {
     const userRole = useSelector((state) => state.host.user.role);
     const pools = useSelector((state) => state.AmazonStore.pools);
+    const currentOwner = useSelector((state) => state.host.user.email);
 
     const dispatch = useDispatch();
     const [open, setOpen] = useState(false);
+
+    const mapPropsToApi = (item, edit) =>
+        edit
+            ? {
+                  description: item.description,
+                  owner: item.owner || currentOwner,
+                  limits: {
+                      storage_size: +item.storageSizeLimit,
+                      objects: +item.objectsLimit,
+                      max_buckets: +item.bucketsLimit,
+                      bucket_storage_size: +item.storageInBucketLimit,
+                      bucket_objects: +item.objectsInBucketLimit,
+                  },
+              }
+            : {
+                  name: item.name,
+                  description: item.description,
+                  owner: item.owner || currentOwner,
+                  default_placement: item.storageType,
+                  limits: {
+                      storage_size: +item.storageSizeLimit,
+                      objects: +item.objectsLimit,
+                      max_buckets: +item.bucketsLimit,
+                      bucket_storage_size: +item.storageInBucketLimit,
+                      bucket_objects: +item.objectsInBucketLimit,
+                  },
+              };
+
+    const mapApiToProps = (item) => ({
+        name: item.name,
+        description: item.description,
+        default_placement: item.default_placement.id,
+        storageSizeLimit: item.stats.storage_size?.limit || 0,
+        objectsLimit: item.stats.objects?.limit || 0,
+        bucketsLimit: +item.stats.buckets?.limit || 0,
+        storageInBucketLimit: item.stats.storage_bucket_limit,
+        objectsInBucketLimit: item.stats.object_bucket_limit,
+        owner: item.owner,
+    });
 
     const handleClose = useCallback(() => {
         setOpen(false);
@@ -63,7 +64,6 @@ const UserModal = ({ user, edit, t }) => {
     const onSubmit = useCallback(
         (values) => {
             handleClose();
-
             let payload = mapPropsToApi(values, edit);
             if (edit) {
                 dispatch(editS3userAndFetch(user.id, payload));
@@ -108,14 +108,7 @@ const UserModal = ({ user, edit, t }) => {
                                     pools={pools}
                                 />
                             ) : (
-                                <UserForm
-                                    t={t}
-                                    open={open}
-                                    handleClose={handleClose}
-                                    onSubmit={onSubmit}
-                                    isAdmin={userRole === 'admin'}
-                                    pools={pools}
-                                />
+                                <UserForm t={t} open={open} handleClose={handleClose} onSubmit={onSubmit} isAdmin={userRole === 'admin'} pools={pools} />
                             )
                         }
                     </Modal.Content>
