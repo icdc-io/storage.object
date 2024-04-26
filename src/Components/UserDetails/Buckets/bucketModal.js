8e573ad69c3265e18d4757b4f870702484c2ad74
DValashchuk@ibagroup.eu <DValashchuk@ibagroup.eu>
2024-04-26 17:15:54 +0300

    Remove logs

--- src/Components/UserDetails/Buckets/bucketModal.js
+++ src/Components/UserDetails/Buckets/bucketModal.js
@@ -39,13 +39,8 @@ const BucketModal = ({ t, bucket, edit }) => {
             let payload = mapPropsToApi(values);
 
             if (edit) {
-                console.log('edit')
-                console.log(payload)
-                console.log({ ...payload, user_name: s3user.keys.s3.user, bucket_name: bucket.bucket_name })
                 dispatch(editBucketAndFetch(s3user.id, { ...payload, user_name: s3user.keys.s3.user, bucket_name: bucket.bucket_name }));
             } else {
-                console.log('edit')
-                console.log(payload)
                 dispatch(createBucketAndFetch(s3user.id, payload));
             }
 
